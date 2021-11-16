11ad34a87e22f57c091e4c545e9d0eb86577bd44
Tran Nhat Thien <Nguyentanmanh2323@gmail.com>
2021-11-16 22:10:56 +0700

    template Work Space

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,74 +1,60 @@
 /* eslint-disable react/no-children-prop */
 /* eslint-disable react/no-unknown-property */
 import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
+import 'antd/dist/antd.css';
 import './Navbar.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { VideoCameraOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
 
-function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+const Navbar = () => {
+  const [collapsed, setCollapsed] = useState(false);
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
+  const onCollapse = (collapsed: boolean) => {
+    setCollapsed(collapsed);
+    console.log(collapsed);
   };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  window.addEventListener('resize', showButton);
-
+  // collapsible collapsed={collapsed} onCollapse={onCollapse}
   return (
-    <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            <img src="/image/Logo.png" />
-            wReflect
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
-                Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
-                Products
-              </Link>
-            </li>
-
-            <li>
-              <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
-                Sign Up
-              </Link>
-            </li>
-          </ul>
-          {button && (
-            <Button buttonStyle={''} type={undefined} onClick={undefined} buttonSize={''}>
-              SIGN UP
-            </Button>
-          )}
-        </div>
-      </nav>
-    </>
+    <div>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsed={collapsed} onCollapse={onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>WorkSpace</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              TNT
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>wReflect ©2022 </Footer>
+        </Layout>
+      </Layout>
+    </div>
   );
-}
+};
 
 export default Navbar;
