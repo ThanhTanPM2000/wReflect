4d51bc63a267f289c2449b7fc9a9c90459a1e930
ThanhTanNguyen2007 <tan.187pm20569@vanlanguni.vn>
2023-05-29 05:05:37 +0700

    test

--- server/wait-for-postgres.sh
+++ server/wait-for-postgres.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+# wait-for-postgres.sh
 
 set -e
 
 host="$1"
 shift
+port="$1"
+shift
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -c '\q'; do
+until nc -z "$host" "$port"; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
