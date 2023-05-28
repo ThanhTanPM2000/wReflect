b5cc1b9cd0160404bde9b34240e3d0f213670fab
ThanhTanNguyen2007 <tan.187pm20569@vanlanguni.vn>
2023-05-29 06:44:06 +0700

    test

--- client/keyreply-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-exec /opt/bitnami/scripts/nginx/entrypoint.sh "$@"
-exit $?
--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-echo "Waiting for Postgres to start..."
-./wait-for-postgres.sh db:5432
-
 yarn build && yarn start
