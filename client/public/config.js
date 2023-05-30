f6f9953463aaba22f578edcf0dfb4d4033fc09de
ThanhTanNguyen2007 <tan.187pm20569@vanlanguni.vn>
2023-05-30 18:19:23 +0700

    test

--- client/public/config.js
+++ client/public/config.js
@@ -2,6 +2,6 @@ window.appConfig = {
   AUTH0_DOMAIN: 'dev-m0ubghav.us.auth0.com',
   AUTH0_CLIENT_ID: 'cOKA1TYswWF0StunJKOjt1v4OBVWTttA',
   AUTH0_RESPONSE_TYPE: 'code',
-  REDIRECT_URI: 'https://localhost:3000',
+  REDIRECT_URI: 'https://localhost',
   SERVER_BASE_URL: 'http://localhost:4000',
 };
