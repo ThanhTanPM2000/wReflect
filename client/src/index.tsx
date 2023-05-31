a2a8ed5d7ff2901ee7f269ec05010346bf059620
ThanhTanNguyen2007 <tan.187pm20569@vanlanguni.vn>
2023-05-31 13:06:15 +0700

    test

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -20,7 +20,7 @@ const httpLink = new HttpLink({
 });
 
 const wsLink = new WebSocketLink({
-  uri: `${config.SERVER_BASE_URL.replace('http', 'ws')}/graphql`,
+  uri: `${config.SERVER_BASE_URL.replace('http', 'wss')}/graphql`,
   options: {
     reconnect: true,
   },
